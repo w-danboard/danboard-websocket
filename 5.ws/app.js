936c4e80a04ec88a756a954e8adabcf73c38f752
wanglin <wanglin46@lenovo.com>
2020-12-22 14:53:18 +0800

    test

--- 5.ws/app.js
+++ 5.ws/app.js
@@ -12,10 +12,10 @@ let server = new WebSocketServer({
 
 server.on('connection', function (socket) {
   // socket 插座
-  console.log('2.服务端监听到了客户端的连接');
+  console.log('2.服务端监听到了客户端的连接')
   // 监听客户端发过来的消息
   socket.on('message', function (message) {
-    console.log('4.客户端连接过来的消息', message);
+    console.log('4.客户端连接过来的消息', message)
     socket.send('5.服务器说：' + message)
   })
 })
